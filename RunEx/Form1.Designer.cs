f3a96fae948d9ec7cbba8ad1f8df908b79dbbaed
pansystar <wh.bt3.p2@gmail.com>
2018-02-22 01:22:39 +0900

    JScriptをレジストリに追加する処理を追加

--- RunEx/Form1.Designer.cs
+++ RunEx/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RunEx
 {
-	partial class Form1
+	partial class RunEx
 	{
 		/// <summary>
 		/// 必要なデザイナー変数です。
@@ -28,12 +28,63 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.comboBox1.ForeColor = System.Drawing.Color.DimGray;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.ItemHeight = 20;
+			this.comboBox1.Location = new System.Drawing.Point(14, 13);
+			this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(366, 28);
+			this.comboBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(338, 54);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(42, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "開く";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(14, 54);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(72, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "JS更新";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// RunEx
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(392, 89);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.Name = "RunEx";
+			this.Text = "RunEx ver.0.0.1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
 	}
 }
 
--- RunEx/Form1.cs
+++ RunEx/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
 
 namespace RunEx
 {
-	public partial class Form1 : Form
+	public partial class RunEx : Form
 	{
-		public Form1()
+		public RunEx()
 		{
 			InitializeComponent();
+		}
+
+		private void Button2_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("よござんすか？", "", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
+			{
+				return;
+			}
+			var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths", true);
+
+			var scriptPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RunEx\Script\";
+
+			foreach (var n in Directory.EnumerateFiles(scriptPath).Where(s => s.Contains(".js")))
+			{
+				var name = Path.GetFileNameWithoutExtension(n);
+
+				var r = regKey.OpenSubKey(name + ".exe");
+
+				if (r != null && r.Name == (name + ".exe"))
+				{
+					var mes = "元:" + r.GetValue(null) + "\n新:" + n + "\n更新してもよろしいですか？";
+					if (MessageBox.Show(mes, "警告", MessageBoxButtons.YesNo) != DialogResult.No)
+					{
+						continue;
+					}
+				}
+				var sk = regKey.CreateSubKey(name + ".exe", true);
+				sk.SetValue(null, n);
+			}
+
 		}
 	}
 }
